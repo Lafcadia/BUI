3595cd0d853ce58dbd22faf1760a9241d5f4d2fb
ChuishenX <chuishenx@oblivionocean.top>
2024-07-08 20:28:13 +0800

    Reconstruction of the Program

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import sys
 from multiprocessing import Process as P
 import multiprocessing as m
 
+
 def customObserve(path, timer, file_type, q):
     q.start()
     p = P(target=observe, args=(path, timer, file_type))
     p.start()
     p.join()
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -23,13 +25,19 @@ class MainWindow(QMainWindow):
         self.started = False
         self.ui.StartStop.clicked.connect(self.startStopper)
         self.ui.chooser.clicked.connect(self.chooser)
-    
+
     def chooser(self):
         self.path = QFileDialog.getExistingDirectory(self, "Choose the Directory to Monitor", "./")
         QMessageBox.information(self, "Path to monitor is chosen!", "Path to monitor is chosen!")
-    
+
     def startStopper(self):
-        self.file_type = "tar" if self.ui.comboBox.currentText() == "Save as tar" else ("zip" if self.ui.comboBox.currentText() == "Save as zip" else None)
+        ct = self.ui.comboBox.currentText()
+        if ct == "Save as tar":
+            self.file_type = "tar"
+        elif ct == "Save as zip":
+            self.file_type = "zip"
+        else:
+            self.file_type = None
         self.timer = float(self.ui.lineEdit.text())
         if not self.started:
             self.started = True
@@ -41,6 +49,7 @@ class MainWindow(QMainWindow):
             self.ui.StartStop.setText("Start")
             self.q.put(0)
             self.q.get()
+
 
 if __name__ == "__main__":
     m.freeze_support()
